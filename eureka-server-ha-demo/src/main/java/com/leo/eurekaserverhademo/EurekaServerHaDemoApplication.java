d1e4462fd4c1b807b146555ff873e4579ced5413
horsezp <horsezp@163.com>
2017-11-06 11:58:18 +0800

    add eureka server demo

---
+++ eureka-server-ha-demo/src/main/java/com/leo/eurekaserverhademo/EurekaServerHaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.leo.eurekaserverhademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer//这句是重点 不然显示不了界面
+@SpringBootApplication
+public class EurekaServerHaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerHaDemoApplication.class, args);
+	}
+}
--- spring-rest-demo/src/main/java/com/leo/springrestdemo/controller/TetsController.java
+++ spring-rest-demo/src/main/java/com/leo/springrestdemo/controller/TetsController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//Controller 的包必须在Application 的包或者子包里面 否则不会被载入
 public class TetsController {
 	
 	@GetMapping("/test")
