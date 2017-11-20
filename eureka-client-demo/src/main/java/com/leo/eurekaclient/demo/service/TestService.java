87dcd074bf8f93b19aea5d7f6da95f0716724dfe
horsezp <horsezp@163.com>
2017-11-20 14:41:28 +0800

    add new project

--- eureka-client-demo/src/main/java/com/leo/eurekaclient/demo/service/TestService.java
+++ eureka-client-demo/src/main/java/com/leo/eurekaclient/demo/service/TestService.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(value = "spring-rest-demo",fallback=TestServiceHystrix.class)
+//只需要定义接口 Feign 本身集成了 Hystrix 所以可以在这里配置Hystrix 的处理类
+
 public interface TestService {
 	
 	//这里定义的是请求路径
